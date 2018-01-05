46e5560de3dafee3baa476929ef7f6fb5742261a
Dani Roxberry <dani@bitoriented.com>
2018-01-05 13:13:54 -0800

    [fix] searchbar component now renders

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
-import SearchBar from './SearchBar'
+import SearchBar from "./SearchBar";
 // import TheaterContainer from './TheaterContainer'
 // import VideoContainer from './VideoContainer'
-import MovieResultsList from './MovieResultsList'
+// import MovieResultsList from './MovieResultsList'
 
 class Container extends Component {
   state = {
-    searchLocation: '',
+    searchLocation: "",
     movieResults: [],
     theaterResults: [],
     currentMovie: {
-      title: '',
-      rating: '',
-      duration: '',
-      description: ''
+      title: "",
+      rating: "",
+      duration: "",
+      description: ""
     }
-  }
+  };
 
-  componentDidMount() {
-    // get geolocation library
+  // componentDidMount() {
+  //   // get geolocation library
 
-    // use that information to ping fandango api
+  //   // use that information to ping fandango api
 
-    // get theaters in current area
+  //   // get theaters in current area
 
-    // set current Movie to first movie result
-  }
+  //   // set current Movie to first movie result
+  // }
 
   handleInputChange = event => {
     const name = event.target.name;
@@ -36,26 +36,23 @@ class Container extends Component {
   };
 
   handleFormSubmit = event => {
+    // when we submit the form, we're going to submit it to google places API and do something about movies.
     event.preventDefault();
   };
 
-  // when we submit the form, we're going to submit it to google places API and do something about movies.
-
   render() {
-    return ( 
+    return (
       <div>
-        <SearchBar 
+        <SearchBar
           search={this.state.searchLocation}
           handleFormSubmit={this.handleFormSubmit}
           handleInputChange={this.handleInputChange}
-          />
-        <MovieResultsList 
-          results={this.state.movieResults}
-          />
-        
+        />
+       { /*
+        <MovieResultsList results={this.state.movieResults} /> */}
       </div>
     );
   }
 }
 
-export default Container
+export default Container;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SearchBar = props => (
+  <form>
+    <div className="form-group">
+      <label htmlFor="search">Search:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search for a location (example: San Fransisco, CA)"
+        id="search"
+      />
+      <button onClick={props.handleFormSubmit} className="btn btn-primary">
+        Search
+      </button>
+    </div>
+  </form>
+);
+
+export default SearchBar;
