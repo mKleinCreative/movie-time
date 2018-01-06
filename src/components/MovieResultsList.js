bf7a4e401baaed0dc18ad14a39276c94a370e8b4
Michael Klein <miketheklein@gmail.com>
2018-01-05 16:26:46 -0800

    added unique key prop to movie results list

--- src/components/MovieResultsList.js
+++ src/components/MovieResultsList.js
@@ -3,7 +3,7 @@ import React from "react";
 const MovieResultsList = props =>
   <ul className="list-group">
     {props.results.map(result =>
-      <li className="list-group-item" key={result.id}>
+      <li className="list-group-item" key={result.tmsId}>
         <strong>{result.title}</strong>
         <em>{result.genre}</em>
       </li>
